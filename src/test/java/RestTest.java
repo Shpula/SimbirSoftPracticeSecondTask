7d7094e816e7a71ef62b480d1a32d5411957353a
butin <butin@DESKTOP-63BCB89>
2022-02-28 20:40:47 +0400

    done

--- src/test/java/RestTest.java
+++ src/test/java/RestTest.java
@@ -1,46 +1,28 @@
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import steps.FirstCaseSteps;
+import steps.SecondCaseSteps;
 
-import static org.hamcrest.core.IsEqual.equalTo;
 import static io.restassured.RestAssured.given;
 
 public class RestTest {
 
+
     @Test
     public void firstCase() {
-        given()
-                .baseUri("https://reqres.in/api/users")
-                .contentType(ContentType.JSON)
-                .when().get()
-                .then()
-                .statusCode(200)
-                .body("data.find{it.first_name == 'George' & it.last_name == 'Bluth'}.email",
-                        equalTo("george.bluth@reqres.in"));
+        FirstCaseSteps.findEmailByLastNameAndFirstName();
+        FirstCaseSteps.checkCurrectFirstNameFirstCase();
+        FirstCaseSteps.checkCurrectLastNameFirstCase();
+        FirstCaseSteps.checkCurrectEmailFirstCase();
 
-    }
-
-    public int numberOfPages()
-    {
-        return
-        given()
-                .baseUri("https://reqres.in/api/users")
-                .contentType(ContentType.JSON)
-                .when().get()
-                .then()
-                .extract().jsonPath().getInt("total_pages");
     }
 
     @Test
     public void secondCase() {
-        for(int i = 1; i >= numberOfPages() + 1; i++) {
-            given()
-                    .baseUri("https://reqres.in/api/users?page=" + i)
-                    .contentType(ContentType.JSON)
-                    .when().get()
-                    .then()
-                    .statusCode(200)
-                    .body("data.find{it.first_name == 'Michael' & it.last_name == 'Lawson'}.email",
-                            equalTo("michael.lawson@reqres"));
-        }
+        SecondCaseSteps.findEmailByLastNameAndFirstNameWithPagination();
+        SecondCaseSteps.checkCurrectFirstNameSecondCase();
+        SecondCaseSteps.checkCurrectLastNameSecondCase();
+        SecondCaseSteps.checkCurrectEmailSecondCase();
+        SecondCaseSteps.checkCurrectCountPages();
     }
 }
+
